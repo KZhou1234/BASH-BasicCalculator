7bd0bdfc417174ac1da931f217ffa4f0bd7beebe
Ubuntu <ubuntu@ip-172-31-35-109.ec2.internal>
2024-08-10 02:27:01 +0000

    basic calculator for two integers calculating

---
+++ basic-calculator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#The script should prompt the user to enter two integers.
+echo "Please enter the first number:"
+read num1
+echo "Please enter the second number:"
+read num2
+#The script should prompt the user to choose an operation: +,-,*,/
+echo "Please select an operator from '+','-','*','/':"
+read operator
+
+#Use conditionals ('if`, `elif`, `else') to perform the chosen operation.
+if [ "$operator" = "+" ]
+then
+  result=$((num1 + num2))
+elif [ "$operator" = "-" ]
+then
+  result=$((num1 - num2))
+elif [ "$operator" = "*" ]
+then
+  result=$((num1 * num2))
+
+elif [ "$operator" = "/" ]
+then
+  if [ $num2 -eq 0 ] #Handle division by zero with an appropriate error message.
+  then
+    echo "The divisor can not be zero!"
+    exit 1
+  else
+  result=$((num1 / num2))
+  fi
+else
+  echo "Please select a valid operator."
+fi
+
+#Display the result of the operation.
+echo "The result is $result."
+
